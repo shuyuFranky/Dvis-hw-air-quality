9505fed140fcd318c3b0e6e5a21f91df649a9440
shuyuFranky <shuyu_@pku.edu.cn>
2017-12-13 21:18:40 +0800

    base 0.0.1

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var getData = require('../public/js/getData');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //var data = getData();
   res.render('index', { title: 'Express' });
 });
 
